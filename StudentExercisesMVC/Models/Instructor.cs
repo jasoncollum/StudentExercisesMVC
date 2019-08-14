943ca0008e7d8bd3ea28caa30b86451d960e4a4b
unknown <jasoncollum@gmail.com>
2019-08-14 07:13:53 -0700

    first commit

---
+++ StudentExercisesMVC/Models/Instructor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesMVC.Models
+{
+    public class Instructor
+    {
+        [Required]
+        [Display(Name = "Instructor Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Slack Handle")]
+        public string SlackHandle { get; set; }
+
+        [Required]
+        [Display(Name = "Specialty")]
+        public string Speacialty { get; set; }
+
+        [Required]
+        [Display(Name = "Cohort Id")]
+        public int CohortId { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
